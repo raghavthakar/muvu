37bdab262765eb92742c5fbb431f31ec961e5039
Raghav <raghavthakar12@gmail.com>
2021-05-23 17:21:47 +0530

    Service to move to point

---
+++ src/muvu_control/src/move_distance_client.cpp
@@ -0,0 +1,51 @@
+#include "ros/ros.h"
+#include "muvu_control/MoveDistance.h"
+
+int main(int argc, char **argv)
+{
+  geometry_msgs::Point target_point;
+  target_point.x=10;
+  target_point.y=10;
+
+  ros::init(argc, argv, "move_distance_client");
+  ros::NodeHandle n;
+
+  ros::ServiceClient client = n.serviceClient
+                    <muvu_control::MoveDistance>("move_distance");
+
+  //Instantiate the service class
+  muvu_control::MoveDistance srv;
+
+  //Inout the request value
+  srv.request.target = target_point;
+
+  //Call the actual service. returns true if succeeded
+  if (client.call(srv))
+  {
+    ROS_INFO("Called the service, and done");
+  }
+  else
+  {
+    ROS_ERROR("Failed to call service move_distance");
+    return 1;
+  }
+
+  target_point.x=2;
+  target_point.y=0;
+
+  //Inout the request value
+  srv.request.target = target_point;
+
+  //Call the actual service. returns true if succeeded
+  if (client.call(srv))
+  {
+    ROS_INFO("Called the service, and done");
+  }
+  else
+  {
+    ROS_ERROR("Failed to call service move_distance");
+    return 1;
+  }
+
+  return 0;
+}
---
+++ src/muvu_control/src/move_distance_server.cpp
@@ -0,0 +1,124 @@
+#include "ros/ros.h"
+#include "muvu_control/MoveDistance.h"
+#include "geometry_msgs/Pose.h"
+#include "geometry_msgs/Twist.h"
+#include "nav_msgs/Odometry.h"
+#include "tf/transform_datatypes.h"
+#include "tf/LinearMath/Matrix3x3.h"
+#include "std_msgs/Float64.h"
+#include <string>
+#include <cmath>
+
+
+class Mover
+{
+  ros::NodeHandle node_handle;
+  std::string odom_topic="/muvu/odom";
+  nav_msgs::OdometryConstPtr current_odom;
+  ros::ServiceServer move_server;
+  ros::Publisher twist_publisher;
+  geometry_msgs::Twist twist_message;
+
+  std::vector<double> getOrientation()
+  {
+    double roll, pitch, yaw;
+    std::vector<double> angles;
+
+    //Get the current odom data of the robot
+    current_odom=ros::topic::waitForMessage<nav_msgs::Odometry>(odom_topic);
+
+    // the incoming geometry_msgs::Quaternion is transformed to a tf::Quaterion
+    tf::Quaternion quat;
+    tf::quaternionMsgToTF(current_odom->pose.pose.orientation, quat);
+
+    // the tf::Quaternion has a method to acess roll pitch and yaw
+    tf::Matrix3x3(quat).getRPY(roll, pitch, yaw);
+
+    angles.push_back(roll);
+    angles.push_back(pitch);
+    angles.push_back(yaw);
+
+    return angles;
+  }
+
+  double distanceFrom(double target_x, double target_y)
+  {
+    return sqrt(pow(target_y-current_odom->pose.pose.position.y, 2)
+              +pow(target_x-current_odom->pose.pose.position.x, 2));
+  }
+
+public:
+  Mover()
+  {
+    move_server = node_handle.advertiseService("move_distance",
+                                    &Mover::move, this);
+
+    twist_publisher = node_handle.advertise
+                      <geometry_msgs::Twist>("/muvu/cmd_vel", 10);
+  }
+
+  bool move(muvu_control::MoveDistance::Request &request,
+            muvu_control::MoveDistance::Response &response)
+  {
+    //Get the current orientation of the robot in space (angles)
+    // [roll, pitch, yaw]
+    std::vector<double> orientation=getOrientation();
+
+    //Find the angle to rotate to face target
+    double angle_to_target = atan((request.target.y-current_odom->
+                                  pose.pose.position.y)/
+                                  (request.target.x-current_odom->
+                                  pose.pose.position.x));
+
+    //ROtate the robot till we face the target
+    while(fabs(angle_to_target-orientation[2])>0.01)
+    {
+      twist_message.angular.z=0.1;
+      twist_message.linear.x=0;
+      twist_publisher.publish(twist_message);
+
+      orientation=getOrientation();
+
+      //Find the angle to rotate to face target
+      angle_to_target = atan((request.target.y-current_odom->
+                                    pose.pose.position.y)/
+                                    (request.target.x-current_odom->
+                                    pose.pose.position.x));
+    }
+
+    //Stop the robot
+    twist_message.angular.z=0;
+    twist_message.linear.x=0;
+    twist_publisher.publish(twist_message);
+
+    //Move the robot forwards till we reach the target
+    while(fabs(distanceFrom(request.target.x, request.target.y))>0.1)
+    {
+      //Get the current odom data of the robot
+      current_odom=ros::topic::waitForMessage<nav_msgs::Odometry>(odom_topic);
+
+      if(fabs(distanceFrom(request.target.x, request.target.y))>2)
+        twist_message.linear.x=0.5;
+      else
+        twist_message.linear.x=0.1;
+
+      twist_publisher.publish(twist_message);
+      ROS_INFO("%f", distanceFrom(request.target.x, request.target.y));
+    }
+
+    //Stop the robot
+    twist_message.angular.z=0;
+    twist_message.linear.x=0;
+    twist_publisher.publish(twist_message);
+
+    return true;
+  }
+};
+
+int main(int argc, char** argv)
+{
+  ros::init(argc, argv, "move_distance_server");
+  Mover mover;
+  ros::spin();
+  return 0;
+}
