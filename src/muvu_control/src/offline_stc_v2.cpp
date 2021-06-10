8edcfbf62874c1e499bc28c0fb831a95fd0e7e4f
Raghav <raghavthakar12@gmail.com>
2021-06-10 22:10:12 +0530

    Spanning tree done in offline stc

--- src/muvu_control/src/offline_stc_v2.cpp
+++ src/muvu_control/src/offline_stc_v2.cpp
@@ -4,10 +4,52 @@
 #define MAP_WIDTH 1000
 #define MAP_HEIGHT 1000
 #define CELL_UNIT 100
-#define SUBCELL_UNIT 50
+#define SUBUNIT_CELL 50
 
 //To simplify accessing neighbours in these directions
 enum {POS_X, NEG_Y, NEG_X, POS_Y};
+
+//To simplify subcell identification
+enum {TOP_RIGHT, TOP_LEFT, BOTTOM_LEFT, BOTTOM_RIGHT};
+
+//Holds the data of a single subcell in the Group
+struct SubCell
+{
+  int x;
+  int y;
+
+  void display()
+  {
+    std::cout<<"X: "<<x<<", Y: "<<y<<std::endl;
+  }
+
+  bool operator==(const SubCell& subcell)
+  {
+    return (x==subcell.x && y==subcell.y);
+  }
+};
+
+class SubCellGroup
+{
+public:
+  //Define an array of 4 subcells {TOP_RIGHT< TOP_LEFT, BOTTOM_LEFT, BOTTOM_RIGHT}
+  SubCell subcells[4];
+
+  SubCellGroup(int parent_x, int parent_y)
+  {
+    subcells[TOP_RIGHT].x=parent_x+SUBUNIT_CELL/2;
+    subcells[TOP_RIGHT].y=parent_y-SUBUNIT_CELL/2;
+
+    subcells[TOP_LEFT].x=parent_x-SUBUNIT_CELL/2;
+    subcells[TOP_LEFT].y=parent_y-SUBUNIT_CELL/2;
+
+    subcells[BOTTOM_LEFT].x=parent_x-SUBUNIT_CELL/2;
+    subcells[BOTTOM_LEFT].y=parent_y+SUBUNIT_CELL/2;
+
+    subcells[BOTTOM_RIGHT].x=parent_x+SUBUNIT_CELL/2;
+    subcells[BOTTOM_RIGHT].y=parent_y+SUBUNIT_CELL/2;
+  }
+};
 
 class Cell
 {
@@ -17,6 +59,8 @@ class Cell
   bool visited=false;
 
 public:
+  SubCellGroup* child_subcellgroup;
+
   //array of 4 pointers to Cell to hold neighbours
   Cell* neighbours[4] = {NULL, NULL, NULL, NULL};
 
@@ -62,6 +106,13 @@ public:
   {
     neighbours[dir]=&(*neighbour);
   }
+
+  //Sets a pointer to corresponding subcell group
+  void setSubCellGroup(SubCellGroup* child_subcellgroup)
+  {
+    this->child_subcellgroup=child_subcellgroup;
+  }
+
 };
 
 class STC_handler
@@ -102,6 +153,8 @@ class STC_handler
     return all_cells.end();
   }
 
+  //Stores the current subcell data
+
 public:
   //constructor sets up the graph
   STC_handler(Cell start_cell)
@@ -114,8 +167,8 @@ public:
       for(int ii=start_cell.getX(); ii<MAP_WIDTH; ii+=CELL_UNIT)
       {
         //if within map boundaries
-        if(i-SUBCELL_UNIT>=0 && i+SUBCELL_UNIT<=MAP_HEIGHT
-          && ii-SUBCELL_UNIT>=0 && ii+SUBCELL_UNIT<=MAP_WIDTH)
+        if(i-SUBUNIT_CELL>=0 && i+SUBUNIT_CELL<=MAP_HEIGHT
+          && ii-SUBUNIT_CELL>=0 && ii+SUBUNIT_CELL<=MAP_WIDTH)
         {
           //Add to list of all cells
           all_cells.push_back(Cell(ii, i));
@@ -197,19 +250,26 @@ public:
     }
   }
 
-  //returns an iterator pointing to the first cell
+  //returns a pointer to the first cell
   Cell* getAllCellsBegin()
   {
     return &(*all_cells.begin());
   }
 
+  //returns an iterator pointing to the first element of spanning tree cells
+  std::list<Cell*>::iterator getSpanningTreeCellsBegin()
+  {
+    return spanning_tree_cells.begin();
+  }
+
+  //Traverses the graph using DFS and n iteratorupdates spanning tree cells list
   void DFS(Cell* prev_cell, Cell* curr_cell)
   {
-    curr_cell->display();
+    // curr_cell->display();
     curr_cell->markVisited();
     spanning_tree_cells.push_back(curr_cell);
 
-    std::cout << "-----------" << '\n';
+    // std::cout << "-----------" << '\n';
 
     for(Cell* neighbour:curr_cell->neighbours)
     {
@@ -217,12 +277,71 @@ public:
         if(!neighbour->isVisited()) DFS(curr_cell, neighbour);
     }
   }
+
+  //divide all the cells in the grpah into subcells
+  void divideIntoSubcells(std::list<Cell*>::iterator spanning_tree_cell)
+  {
+    if(spanning_tree_cell==spanning_tree_cells.end())
+      return;
+
+    // (*spanning_tree_cell)->display();
+
+    //Create a subcellgroup for the current spanning tree cell
+    SubCellGroup *temp_scg=new SubCellGroup((*spanning_tree_cell)->getX(),
+                              (*spanning_tree_cell)->getY());
+
+    //Set the subcell group in the corresponding spanning tree cell
+    (*spanning_tree_cell)->setSubCellGroup(temp_scg);
+    divideIntoSubcells(++spanning_tree_cell);
+  }
+
+  void showSpanningTreeWithSubCellGroups()
+  {
+    for(auto i:spanning_tree_cells)
+    {
+      std::cout << "CURRENT CELL: " << '\n';
+      i->display();
+      std::cout << "SUBCELLS: " << '\n';
+      for(int ii=0; ii<4; ii++)
+      {
+        i->child_subcellgroup->subcells[ii].display();
+      }
+    }
+  }
+
+  // recursive function to circumnavigate the spanning tree
+  void circumnavigate(Cell* curr_cell, Cell* next_cell, SubCell* curr_subcell,
+                      std::list<Cell*>::iterator spanning_tree_cell, int count)
+  {
+    if(spanning_tree_cell==spanning_tree_cells.end()||count==5)
+      return;
+
+    std::cout << "CURRENT CELL: " << '\n';
+    curr_cell->display();
+
+    //if the current subcell is the top right subcell,
+    //try going up to next cell, else right to same
+    if(*curr_subcell==curr_cell->child_subcellgroup->subcells[TOP_RIGHT])
+      curr_subcell->display();
+
+    circumnavigate(next_cell, *(++spanning_tree_cell), curr_subcell, spanning_tree_cell, count+1);
+  }
 };
 
 int main()
 {
   STC_handler handler(Cell(50, 50));
-  handler.showCells();
+  // handler.showCells();
   handler.DFS(handler.getAllCellsBegin(), handler.getAllCellsBegin());
-  handler.showSpanningTree();
+  // handler.showSpanningTree();
+  handler.divideIntoSubcells(handler.getSpanningTreeCellsBegin());
+  // handler.showSpanningTreeWithSubCellGroups();
+
+  auto st_cell_iterator=handler.getSpanningTreeCellsBegin();
+  auto curr_cell=*st_cell_iterator;
+  SubCell* curr_subcell=&curr_cell->child_subcellgroup->subcells[0];
+  st_cell_iterator++;
+  auto next_cell=*st_cell_iterator;
+
+  handler.circumnavigate(curr_cell, next_cell, curr_subcell, st_cell_iterator, 0);
 }
